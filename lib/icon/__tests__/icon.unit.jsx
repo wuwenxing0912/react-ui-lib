8481b0d5f4a84d57e2a27736073ab2aa9487c942
wuwenxing <1428560819@qq.com>
2021-06-05 22:48:32 +0800

    add icon unit test

---
+++ lib/icon/__tests__/icon.unit.jsx
@@ -0,0 +1,18 @@
+import * as renderer from 'react-test-renderer';
+import React from 'react';
+import Icon from '../icon';
+import {mount} from 'enzyme';
+
+describe('Icon', () => {
+  it('render 成功', () => {
+    const json = renderer.create(<Icon />).toJSON()
+    expect(json).toMatchSnapshot()
+  });
+
+  it('onClick', () => {
+    const fn = jest.fn();
+    const component = mount(<Icon name='disable' onClick={fn}/>);
+    component.find('svg').simulate('click');
+    expect(fn).toBeCalled();
+  });
+})
