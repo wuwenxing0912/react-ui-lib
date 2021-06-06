ed30002ef68a9d585276b51d8b0b0c46ede766fe
wuwenxing <1428560819@qq.com>
2021-06-06 11:25:21 +0800

    add jest config ci

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,10 @@
 module.exports = {
   verbose: true,
   clearMocks: false,
-  reporters: ["default", "jest-junit"],
-  collectCoverage: true,
-  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
+  reporters: ["default"],
+
+  collectCoverage: false,
+
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleDirectories: ['node_modules'],
   globals: {
