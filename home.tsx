a33cd9837f28c8b9f574c0be5551df5d6c264012
wuwenxing <1428560819@qq.com>
2021-06-14 17:26:54 +0800

    完善 aside content footer

--- home.tsx
+++ home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
 import Icon from "./lib/icon/icon";
 import Button from "./lib/button.home";
 import Dialog from "./lib/dialog/dialog.home";
@@ -17,37 +17,40 @@ ReactDOM.render(
 					<span className="text">X-UI</span>
 				</div>
 				<div className="gh">
-				<a href="https://github.com/wuwenxing0912/react-ui-lib" target='blank'>
-					<Icon name='github' className='gh-icon'></Icon>
-				</a>
+					<a href="https://github.com/wuwenxing0912/react-ui-lib" target="blank" >
+						<Icon name="github" className="gh-icon"></Icon>
+					</a>
 				</div>
 			</Header>
 			<Layout>
-				<Aside style={{ border: "1px solid blue" }}>
+				<Aside className="home-aside">
 					<h2>组件</h2>
 					<ul>
 						<li>
-							<Link to="/icon">Icon</Link>
+							<NavLink to="/icon">Icon</NavLink>
 						</li>
 						<li>
-							<Link to="/button">Button</Link>
+							<NavLink to="/button">Button</NavLink>
 						</li>
 						<li>
-							<Link to="/dialog">Dialog</Link>
+							<NavLink to="/dialog">Dialog</NavLink>
 						</li>
 						<li>
-							<Link to="/layout">Layout</Link>
+							<NavLink to="/layout">Layout</NavLink>
 						</li>
 					</ul>
 				</Aside>
-				<Content>
+				<Content className='home-content'>
 					<Route path="/icon" component={Icon}></Route>
 					<Route path="/button" component={Button}></Route>
 					<Route path="/dialog" component={Dialog}></Route>
 					<Route path="/layout" component={LayoutExample}></Route>
 				</Content>
 			</Layout>
-			<Footer style={{ border: "1px solid black" }}>Footer</Footer>
+			<Footer className="home-footer">
+				<span>&copy; xing</span>
+				<span className="text">X-UI</span>
+			</Footer>
 		</Layout>
 	</Router>,
 	document.querySelector("#root")
