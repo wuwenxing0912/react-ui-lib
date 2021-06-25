cbd53073f1e9a19fc76b11bb1881661bdb4deb69
wuwenxing <1428560819@qq.com>
2021-06-26 00:10:03 +0800

    添加 home 页 input API

--- lib/input/input.home.tsx
+++ lib/input/input.home.tsx
@@ -13,6 +13,78 @@ const inputHome: React.FunctionComponent = () => {
       {createSection("组件和代码示例", defaultInput, "输入框的基本用法", {placeholder: "placeholder"})}
       {createSection("", disabledInput, "输入框禁用状态", {placeholder: "disabled", disabled: true})}
       {createSection("", fixInput, "带有前/后缀图标的输入框", {prefix: "modify", suffix: "modify"})}
+      <section>
+				<h1>API</h1>
+				<table className="api-table" cellSpacing="0" cellPadding="0">
+					<thead>
+						<tr>
+							<th className="api-table-head">API</th>
+							<th className="api-table-head">说明</th>
+							<th className="api-table-head">类型</th>
+							<th className="api-table-head">是否可选</th>
+							<th className="api-table-head">默认值</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td className="api-button-type">id</td>
+							<td className="api-type-description">输入框的id</td>
+							<td className="api-type">string</td>
+							<td className="api-nesscery">是</td>
+							<td className="api-default">---</td>
+						</tr>
+						<tr>
+							<td className="api-classname">placeholder</td>
+							<td className="api-classname-description">输入框的placeholder</td>
+							<td className="api-type">string</td>
+							<td className="api-nesscery">是</td>
+							<td className="api-default">---</td>
+						</tr>
+						<tr>
+							<td className="api-classname">className</td>
+							<td className="api-classname-description">自定义类名</td>
+							<td className="api-type">string</td>
+							<td className="api-nesscery">是</td>
+							<td className="api-default">---</td>
+						</tr>
+						<tr>
+							<td className="api-style">style</td>
+							<td className="api-style-description">自定义样式</td>
+							<td className="api-type">React.CSSProperties</td>
+							<td className="api-nesscery">是</td>
+							<td className="api-default">---</td>
+						</tr>
+            <tr>
+							<td className="api-style">value</td>
+							<td className="api-style-description">输入框的value</td>
+							<td className="api-type">string</td>
+							<td className="api-nesscery">是</td>
+							<td className="api-default">---</td>
+						</tr>
+						<tr>
+							<td className="api-style">disabled</td>
+							<td className="api-style-description">输入框禁用状态</td>
+							<td className="api-type">boolean</td>
+							<td className="api-nesscery">是</td>
+							<td className="api-default">false</td>
+						</tr>
+						<tr>
+							<td className="api-style">prefix</td>
+							<td className="api-style-description">输入框前缀图标</td>
+							<td className="api-type">string</td>
+							<td className="api-nesscery">是</td>
+							<td className="api-default">---</td>
+						</tr>
+						<tr>
+							<td className="api-style">suffix</td>
+							<td className="api-style-description">输入框后缀图标</td>
+							<td className="api-type">string</td>
+							<td className="api-nesscery">是</td>
+							<td className="api-default">---</td>
+						</tr>
+					</tbody>
+				</table>
+			</section>
     </Fragment>
   )
 };
