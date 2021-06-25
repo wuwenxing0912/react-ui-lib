ab54f03bf8dfb60ed91e7a3e6c4e7bea2e77a36b
wuwenxing <1428560819@qq.com>
2021-06-25 22:28:37 +0800

    添加 disabled prefix suffix 接口

--- lib/input/input.tsx
+++ lib/input/input.tsx
@@ -1,16 +1,43 @@
 import React from "react";
 import classes from "../helpers/classes";
-import './input.scss';
+import Icon from "../icon/icon";
+import "./input.scss";
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
-
+	id?: string;
+	placeholder?: string;
+	className?: string;
+	style?: React.CSSProperties;
+	value?: string;
+	disabled?: boolean;
+	prefix?: string;
+	suffix?: string;
 }
 
 const Input: React.FunctionComponent<Props> = (props) => {
-  const {className, ...rest} = props;
-  return (
-    <input className={classes('x-input', className)} {...rest}/>
-  )
+	const { className, value, disabled, prefix, suffix, ...rest } = props;
+	const prefixClassName = prefix && "prefix";
+	const suffixClassName = suffix && "suffix";
+	const inputWrapperClassName = classes("x-input-wrapper", className, prefixClassName, suffixClassName);
+	return (
+		<span className={inputWrapperClassName}>
+			{prefix ? (
+				<span className="prefix-wrapper">
+					<Icon name={prefix} className="prefix-icon" />
+				</span>
+			) : (
+				""
+			)}
+			<input value={value} className="x-input" disabled={disabled} {...rest} />
+			{suffix ? (
+				<span className="suffix-wrapper">
+					<Icon name={suffix} className="suffix-icon" />
+				</span>
+			) : (
+				""
+			)}
+		</span>
+	);
 };
 
 export default Input;
