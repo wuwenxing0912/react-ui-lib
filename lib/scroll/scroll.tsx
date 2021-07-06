c6a45883617c294a1ec9b7aac41f587f25530c0f
wuwenxing <1428560819@qq.com>
2021-07-06 11:05:13 +0800

    添加滚动条 计算滚动条滚动位置

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { UIEventHandler, useEffect, useRef, useState } from "react";
 import classes from "../helpers/classes";
 import scrollbarWidth from "./scroll-width";
 import "./scroll.scss";
@@ -9,9 +9,40 @@ interface Props {
 
 const Scroll: React.FunctionComponent<Props> = (props) => {
 	const { ...rest } = props;
+	const [barHeight, setBarHeight] = useState(0);
+	const [barTop, setBarTop] = useState(0);
+	const onScroll: UIEventHandler = (e) => {
+		const scrollHeight = scrollInnerRef.current!.scrollHeight;
+		const viewHeight = scrollInnerRef.current!.getBoundingClientRect().height;
+		const scrollTop = scrollInnerRef.current!.scrollTop;
+		setBarTop((scrollTop * viewHeight) / scrollHeight);
+	};
+
+	const scrollInnerRef = useRef<HTMLDivElement>(null);
+	useEffect(() => {
+		const scrollHeight = scrollInnerRef.current!.scrollHeight;
+		const viewHeight = scrollInnerRef.current!.getBoundingClientRect().height;
+		setBarHeight((viewHeight * viewHeight) / scrollHeight);
+	}, []);
 	return (
 		<div className={classes("x-scroll")} {...rest}>
-			<div className="x-scroll-inner" style={{right: -scrollbarWidth()}}>{props.children}</div>
+			<div
+				className="x-scroll-inner"
+				style={{ right: -scrollbarWidth() }}
+				ref={scrollInnerRef}
+				onScroll={onScroll}
+			>
+				{props.children}
+			</div>
+			<div className="x-scroll-track">
+				<div
+					className="x-scroll-bar"
+					style={{
+						height: `${barHeight}px`,
+						transform: `translateY(${barTop}px)`
+					}}
+				></div>
+			</div>
 		</div>
 	);
 };
