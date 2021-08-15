3782c56ab90551a5a01ec8243cd47ce3f3a7a001
wuwenxing <1428560819@qq.com>
2021-08-15 11:31:58 +0800

    格式化代码

--- lib/button/button.home.tsx
+++ lib/button/button.home.tsx
@@ -30,288 +30,288 @@ const buttonSize = `const [size, setSize] = useState("middle");
 <Button type="danger" onClick={() => {setSize("small")}}>Danger</Button>`;
 
 const ButtonHome: React.FunctionComponent = () => {
-	const section1 = "按钮有四种类型：默认按钮、主题按钮、虚线按钮以及危险按钮。";
-	const section2 = "按钮禁用：设置按钮 disabled 禁用状态。";
-	const section3 = "图标按钮：设置 icon 属性。";
-	const section4 = "幽灵按钮：常用在有色背景上，按钮背景变为透明。";
-	const section5 = "按钮尺寸：按钮有大、中、小三种尺寸。";
-	return (
-		<Fragment>
-			{createSection("组件和代码示例", false, buttonType, section1)}
-			{createSection("", true, buttonDisabled, section2)}
-			{createSection("", false, buttonGhost, section4, [], true)}
-			{createSectionButtonSize("", buttonSize, section5)}
-			{createSection("", false, buttonIcon, section3, [
-				"setting",
-				"modify",
-				"sign-out",
-				"warn",
-			])}
-			<section>
-				<h1>API</h1>
-				<table className="api-table" cellSpacing="0" cellPadding="0">
-					<thead>
-						<tr>
-							<th className="api-table-head">API</th>
-							<th className="api-table-head">说明</th>
-							<th className="api-table-head">类型</th>
-							<th className="api-table-head">是否可选</th>
-							<th className="api-table-head">默认值</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td className="api-button-type">type</td>
-							<td className="api-type-description">设置button的样式</td>
-							<td className="api-type">
-								"default" | "primary" | "dashed" | "danger"
-							</td>
-							<td className="api-nesscery">是</td>
-							<td className="api-default">default</td>
-						</tr>
-						<tr>
-							<td className="api-classname">className</td>
-							<td className="api-classname-description">自定义类名</td>
-							<td className="api-type">string</td>
-							<td className="api-nesscery">是</td>
-							<td className="api-default">---</td>
-						</tr>
-						<tr>
-							<td className="api-style">style</td>
-							<td className="api-style-description">自定义样式</td>
-							<td className="api-type">React.CSSProperties</td>
-							<td className="api-nesscery">是</td>
-							<td className="api-default">---</td>
-						</tr>
-						<tr>
-							<td className="api-style">disabled</td>
-							<td className="api-style-description">按钮禁用状态</td>
-							<td className="api-type">boolean</td>
-							<td className="api-nesscery">是</td>
-							<td className="api-default">false</td>
-						</tr>
-						<tr>
-							<td className="api-style">ghost</td>
-							<td className="api-style-description">幽灵按钮，背景透明</td>
-							<td className="api-type">boolean</td>
-							<td className="api-nesscery">是</td>
-							<td className="api-default">false</td>
-						</tr>
-						<tr>
-							<td className="api-style">size</td>
-							<td className="api-style-description">按钮尺寸大小</td>
-							<td className="api-type">"large" | "middle" | "small"</td>
-							<td className="api-nesscery">是</td>
-							<td className="api-default">middle</td>
-						</tr>
-						<tr>
-							<td className="api-style">icon</td>
-							<td className="api-style-description">自定义按钮图标</td>
-							<td className="api-type">string</td>
-							<td className="api-nesscery">是</td>
-							<td className="api-default">---</td>
-						</tr>
-						<tr>
-							<td className="api-style">onClick</td>
-							<td className="api-style-description">点击按钮时的回调</td>
-							<td className="api-type">React.MouseEventHandler</td>
-							<td className="api-nesscery">是</td>
-							<td className="api-default">---</td>
-						</tr>
-						<tr>
-							<td className="api-style">buttonType</td>
-							<td className="api-style-description">
-								设置原生button的type属性
-							</td>
-							<td className="api-type">"button" | "submit" | "reset"</td>
-							<td className="api-nesscery">是</td>
-							<td className="api-default">button</td>
-						</tr>
-					</tbody>
-				</table>
-			</section>
-		</Fragment>
-	);
+  const section1 = "按钮有四种类型：默认按钮、主题按钮、虚线按钮以及危险按钮。";
+  const section2 = "按钮禁用：设置按钮 disabled 禁用状态。";
+  const section3 = "图标按钮：设置 icon 属性。";
+  const section4 = "幽灵按钮：常用在有色背景上，按钮背景变为透明。";
+  const section5 = "按钮尺寸：按钮有大、中、小三种尺寸。";
+  return (
+    <Fragment>
+      {createSection("组件和代码示例", false, buttonType, section1)}
+      {createSection("", true, buttonDisabled, section2)}
+      {createSection("", false, buttonGhost, section4, [], true)}
+      {createSectionButtonSize("", buttonSize, section5)}
+      {createSection("", false, buttonIcon, section3, [
+        "setting",
+        "modify",
+        "sign-out",
+        "warn",
+      ])}
+      <section>
+        <h1>API</h1>
+        <table className="api-table" cellSpacing="0" cellPadding="0">
+          <thead>
+            <tr>
+              <th className="api-table-head">API</th>
+              <th className="api-table-head">说明</th>
+              <th className="api-table-head">类型</th>
+              <th className="api-table-head">是否可选</th>
+              <th className="api-table-head">默认值</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="api-button-type">type</td>
+              <td className="api-type-description">设置button的样式</td>
+              <td className="api-type">
+                "default" | "primary" | "dashed" | "danger"
+              </td>
+              <td className="api-nesscery">是</td>
+              <td className="api-default">default</td>
+            </tr>
+            <tr>
+              <td className="api-classname">className</td>
+              <td className="api-classname-description">自定义类名</td>
+              <td className="api-type">string</td>
+              <td className="api-nesscery">是</td>
+              <td className="api-default">---</td>
+            </tr>
+            <tr>
+              <td className="api-style">style</td>
+              <td className="api-style-description">自定义样式</td>
+              <td className="api-type">React.CSSProperties</td>
+              <td className="api-nesscery">是</td>
+              <td className="api-default">---</td>
+            </tr>
+            <tr>
+              <td className="api-style">disabled</td>
+              <td className="api-style-description">按钮禁用状态</td>
+              <td className="api-type">boolean</td>
+              <td className="api-nesscery">是</td>
+              <td className="api-default">false</td>
+            </tr>
+            <tr>
+              <td className="api-style">ghost</td>
+              <td className="api-style-description">幽灵按钮，背景透明</td>
+              <td className="api-type">boolean</td>
+              <td className="api-nesscery">是</td>
+              <td className="api-default">false</td>
+            </tr>
+            <tr>
+              <td className="api-style">size</td>
+              <td className="api-style-description">按钮尺寸大小</td>
+              <td className="api-type">"large" | "middle" | "small"</td>
+              <td className="api-nesscery">是</td>
+              <td className="api-default">middle</td>
+            </tr>
+            <tr>
+              <td className="api-style">icon</td>
+              <td className="api-style-description">自定义按钮图标</td>
+              <td className="api-type">string</td>
+              <td className="api-nesscery">是</td>
+              <td className="api-default">---</td>
+            </tr>
+            <tr>
+              <td className="api-style">onClick</td>
+              <td className="api-style-description">点击按钮时的回调</td>
+              <td className="api-type">React.MouseEventHandler</td>
+              <td className="api-nesscery">是</td>
+              <td className="api-default">---</td>
+            </tr>
+            <tr>
+              <td className="api-style">buttonType</td>
+              <td className="api-style-description">
+                设置原生button的type属性
+              </td>
+              <td className="api-type">"button" | "submit" | "reset"</td>
+              <td className="api-nesscery">是</td>
+              <td className="api-default">button</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </Fragment>
+  );
 };
 
 const createSection = (
-	h1?: string,
-	disabled?: boolean,
-	code?: string,
-	description?: string,
-	icon?: (string | undefined)[],
-	ghost?: boolean
+  h1?: string,
+  disabled?: boolean,
+  code?: string,
+  description?: string,
+  icon?: (string | undefined)[],
+  ghost?: boolean
 ) => {
-	const [visible, setVisble] = useState(false);
-	const [showCode, setshowCode] = useState(false);
-	const f = (e: React.MouseEvent) => {
-		console.log(e.type);
-	};
-	const containerGhostStyle = ghost && {
-		background: "rgb(190, 200, 200)",
-	};
-	return (
-		<Fragment>
-			<section>
-				{h1 ? <h1>{h1}</h1> : ""}
-				<div
-					className="example-container"
-					style={containerGhostStyle as object}
-				>
-					<Button
-						className="default"
-						style={{ marginRight: 20 }}
-						disabled={disabled}
-						icon={icon && icon[1]}
-						ghost={ghost}
-						onClick={f}
-					>
-						Default
-					</Button>
-					<Button
-						type="primary"
-						style={{ marginRight: 20 }}
-						disabled={disabled}
-						icon={icon && icon[1]}
-						ghost={ghost}
-						onClick={f}
-					>
-						Primary
-					</Button>
-					<Button
-						type="dashed"
-						style={{ marginRight: 20 }}
-						disabled={disabled}
-						icon={icon && icon[1]}
-						ghost={ghost}
-						onClick={f}
-					>
-						Dashed
-					</Button>
-					<Button
-						type="danger"
-						disabled={disabled}
-						icon={icon && icon[1]}
-						ghost={ghost}
-						onClick={f}
-					>
-						Danger
-					</Button>
-				</div>
-				<div className="description">
-					<span className="text">{description}</span>
-					<span
-						className="code-icon"
-						style={{ display: "inline-flex", cursor: "pointer" }}
-						onClick={() => {
-							setVisble(!visible);
-						}}
-						onMouseEnter={() => {
-							setshowCode(!showCode);
-						}}
-						onMouseLeave={() => {
-							setshowCode(!showCode);
-						}}
-					>
-						<Icon name="code" style={{ height: 19 }}></Icon>
-					</span>
-					<span
-						className="pop-content"
-						style={{ display: showCode ? "inline-block" : "none" }}
-					>
-						{visible ? "收起代码" : "显示代码"}
-					</span>
-				</div>
-				<div
-					className="code-area"
-					style={{ display: visible ? "block" : "none" }}
-				>
-					{createHighlightCode(code as string)}
-				</div>
-			</section>
-		</Fragment>
-	);
+  const [visible, setVisble] = useState(false);
+  const [showCode, setshowCode] = useState(false);
+  const f = (e: React.MouseEvent) => {
+    console.log(e.type);
+  };
+  const containerGhostStyle = ghost && {
+    background: "rgb(190, 200, 200)",
+  };
+  return (
+    <Fragment>
+      <section>
+        {h1 ? <h1>{h1}</h1> : ""}
+        <div
+          className="example-container"
+          style={containerGhostStyle as object}
+        >
+          <Button
+            className="default"
+            style={{ marginRight: 20 }}
+            disabled={disabled}
+            icon={icon && icon[1]}
+            ghost={ghost}
+            onClick={f}
+          >
+            Default
+          </Button>
+          <Button
+            type="primary"
+            style={{ marginRight: 20 }}
+            disabled={disabled}
+            icon={icon && icon[1]}
+            ghost={ghost}
+            onClick={f}
+          >
+            Primary
+          </Button>
+          <Button
+            type="dashed"
+            style={{ marginRight: 20 }}
+            disabled={disabled}
+            icon={icon && icon[1]}
+            ghost={ghost}
+            onClick={f}
+          >
+            Dashed
+          </Button>
+          <Button
+            type="danger"
+            disabled={disabled}
+            icon={icon && icon[1]}
+            ghost={ghost}
+            onClick={f}
+          >
+            Danger
+          </Button>
+        </div>
+        <div className="description">
+          <span className="text">{description}</span>
+          <span
+            className="code-icon"
+            style={{ display: "inline-flex", cursor: "pointer" }}
+            onClick={() => {
+              setVisble(!visible);
+            }}
+            onMouseEnter={() => {
+              setshowCode(!showCode);
+            }}
+            onMouseLeave={() => {
+              setshowCode(!showCode);
+            }}
+          >
+            <Icon name="code" style={{ height: 19 }}></Icon>
+          </span>
+          <span
+            className="pop-content"
+            style={{ display: showCode ? "inline-block" : "none" }}
+          >
+            {visible ? "收起代码" : "显示代码"}
+          </span>
+        </div>
+        <div
+          className="code-area"
+          style={{ display: visible ? "block" : "none" }}
+        >
+          {createHighlightCode(code as string)}
+        </div>
+      </section>
+    </Fragment>
+  );
 };
 
 const createSectionButtonSize = (
-	h1?: string,
-	code?: string,
-	description?: string
+  h1?: string,
+  code?: string,
+  description?: string
 ) => {
-	const [visible, setVisble] = useState(false);
-	const [showCode, setshowCode] = useState(false);
-	const [size, setSize] = useState("middle");
-	return (
-		<Fragment>
-			<section>
-				{h1 ? <h1>{h1}</h1> : ""}
-				<div className="example-container">
-					<Button
-						style={{ marginRight: 20 }}
-						size={size as "large"}
-						onClick={() => {
-							setSize("large");
-						}}
-					>
-						Large
-					</Button>
-					<Button
-						type="primary"
-						style={{ marginRight: 20 }}
-						size={size as "middle"}
-						onClick={() => setSize("middle")}
-					>
-						Middle
-					</Button>
-					<Button
-						type="dashed"
-						style={{ marginRight: 20 }}
-						size={size as "middle"}
-						onClick={() => setSize("middle")}
-					>
-						Middle
-					</Button>
-					<Button
-						type="danger"
-						size={size as "small"}
-						onClick={() => setSize("small")}
-					>
-						Small
-					</Button>
-				</div>
-				<div className="description">
-					<span className="text">{description}</span>
-					<span
-						className="code-icon"
-						style={{ display: "inline-flex", cursor: "pointer" }}
-						onClick={() => {
-							setVisble(!visible);
-						}}
-						onMouseEnter={() => {
-							setshowCode(!showCode);
-						}}
-						onMouseLeave={() => {
-							setshowCode(!showCode);
-						}}
-					>
-						<Icon name="code" style={{ height: 19 }}></Icon>
-					</span>
-					<span
-						className="pop-content"
-						style={{ display: showCode ? "inline-block" : "none" }}
-					>
-						{visible ? "收起代码" : "显示代码"}
-					</span>
-				</div>
-				<div
-					className="code-area"
-					style={{ display: visible ? "block" : "none" }}
-				>
-					{createHighlightCode(code as string)}
-				</div>
-			</section>
-		</Fragment>
-	);
+  const [visible, setVisble] = useState(false);
+  const [showCode, setshowCode] = useState(false);
+  const [size, setSize] = useState("middle");
+  return (
+    <Fragment>
+      <section>
+        {h1 ? <h1>{h1}</h1> : ""}
+        <div className="example-container">
+          <Button
+            style={{ marginRight: 20 }}
+            size={size as "large"}
+            onClick={() => {
+              setSize("large");
+            }}
+          >
+            Large
+          </Button>
+          <Button
+            type="primary"
+            style={{ marginRight: 20 }}
+            size={size as "middle"}
+            onClick={() => setSize("middle")}
+          >
+            Middle
+          </Button>
+          <Button
+            type="dashed"
+            style={{ marginRight: 20 }}
+            size={size as "middle"}
+            onClick={() => setSize("middle")}
+          >
+            Middle
+          </Button>
+          <Button
+            type="danger"
+            size={size as "small"}
+            onClick={() => setSize("small")}
+          >
+            Small
+          </Button>
+        </div>
+        <div className="description">
+          <span className="text">{description}</span>
+          <span
+            className="code-icon"
+            style={{ display: "inline-flex", cursor: "pointer" }}
+            onClick={() => {
+              setVisble(!visible);
+            }}
+            onMouseEnter={() => {
+              setshowCode(!showCode);
+            }}
+            onMouseLeave={() => {
+              setshowCode(!showCode);
+            }}
+          >
+            <Icon name="code" style={{ height: 19 }}></Icon>
+          </span>
+          <span
+            className="pop-content"
+            style={{ display: showCode ? "inline-block" : "none" }}
+          >
+            {visible ? "收起代码" : "显示代码"}
+          </span>
+        </div>
+        <div
+          className="code-area"
+          style={{ display: visible ? "block" : "none" }}
+        >
+          {createHighlightCode(code as string)}
+        </div>
+      </section>
+    </Fragment>
+  );
 };
 
 export default ButtonHome;
--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -4,46 +4,56 @@ import "./button.scss";
 import Icon from "../icon/icon";
 
 interface Props {
-	type?: "default" | "primary" | "dashed" | "danger";
-	className?: string;
-	style?: React.CSSProperties;
-	disabled?: boolean;
-	icon?: string;
-	onClick?: React.MouseEventHandler;
-	ghost?: boolean,
-	size?: "large" | "middle" | "small",
-	buttonType?: "button" | "submit" | "reset"
+  type?: "default" | "primary" | "dashed" | "danger";
+  className?: string;
+  style?: React.CSSProperties;
+  disabled?: boolean;
+  icon?: string;
+  onClick?: React.MouseEventHandler;
+  ghost?: boolean;
+  size?: "large" | "middle" | "small";
+  buttonType?: "button" | "submit" | "reset";
 }
 
 const Button: React.FunctionComponent<Props> = (props) => {
-	const { type, className, buttonType, ...rest} = props;
-	const disabled = props.disabled ? "disabled" : undefined;
-	const ghost = props.ghost ? "ghost" : undefined;
-	const size = props.size ? props.size : "middle";
-	const icon = props.icon && (
-		<Icon name={props.icon}></Icon>
-	);
-	const onClick = (e: React.MouseEvent) => {
-		if (disabled) return e.preventDefault();
-		props.onClick && props.onClick.call(e.target, e);
-	}
-	const buttonClassName = classes("x-button", type, className, disabled, ghost, size);
-	return (
-		<Fragment>
-			<button className={buttonClassName} onClick={onClick} type={buttonType} {...rest}>
-				{icon}
-				<span className="text">{props.children}</span>
-			</button>
-		</Fragment>
-	);
+  const { type, className, buttonType, ...rest } = props;
+  const disabled = props.disabled ? "disabled" : undefined;
+  const ghost = props.ghost ? "ghost" : undefined;
+  const size = props.size ? props.size : "middle";
+  const icon = props.icon && <Icon name={props.icon}></Icon>;
+  const onClick = (e: React.MouseEvent) => {
+    if (disabled) return e.preventDefault();
+    props.onClick && props.onClick.call(e.target, e);
+  };
+  const buttonClassName = classes(
+    "x-button",
+    type,
+    className,
+    disabled,
+    ghost,
+    size
+  );
+  return (
+    <Fragment>
+      <button
+        className={buttonClassName}
+        onClick={onClick}
+        type={buttonType}
+        {...rest}
+      >
+        {icon}
+        <span className="text">{props.children}</span>
+      </button>
+    </Fragment>
+  );
 };
 
 Button.defaultProps = {
-	type: "default",
-	disabled: false,
-	ghost: false,
-	size: "middle",
-	buttonType: "button"
+  type: "default",
+  disabled: false,
+  ghost: false,
+  size: "middle",
+  buttonType: "button",
 };
 
 export default Button;
