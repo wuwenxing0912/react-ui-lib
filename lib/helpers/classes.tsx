4e4cdca2ff062de65acf69a6513747b375b3a95d
wuwenxing <1428560819@qq.com>
2021-08-16 13:53:25 +0800

    格式化 helpers代码

--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -1,24 +1,24 @@
 function classes(...name: (string | undefined)[]) {
-	return name.filter(Boolean).join(" ");
+  return name.filter(Boolean).join(" ");
 }
 
 export default classes;
 
 interface Options {
-	extra: string | undefined;
+  extra: string | undefined;
 }
 
 interface ClassToggles {
-	[K: string]: boolean;
+  [K: string]: boolean;
 }
 
 const componentClassMaker =
-	(prefix: string) => (name: string | ClassToggles, options?: Options) =>
-		Object.entries(name instanceof Object ? name : { [name]: name })
-			.filter((kv) => kv[1] !== false)
-			.map((kv) => kv[0])
-			.map((name) => [prefix, name].filter(Boolean).join("-"))
-			.concat((options && options.extra) || [])
-			.join(" ");
+  (prefix: string) => (name: string | ClassToggles, options?: Options) =>
+    Object.entries(name instanceof Object ? name : { [name]: name })
+      .filter((kv) => kv[1] !== false)
+      .map((kv) => kv[0])
+      .map((name) => [prefix, name].filter(Boolean).join("-"))
+      .concat((options && options.extra) || [])
+      .join(" ");
 
 export { componentClassMaker };
