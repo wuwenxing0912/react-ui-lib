dcfea5f2a169f98d5994fd9256d8819238db7132
wuwenxing <1428560819@qq.com>
2021-08-16 20:27:19 +0800

    格式化 toast 代码

--- lib/toast/create-toast.tsx
+++ lib/toast/create-toast.tsx
@@ -4,27 +4,27 @@ import classes from "../helpers/classes";
 import "./toast.scss";
 
 export interface Props {
-	className?: string;
-	style?: React.CSSProperties;
-	type?: "information" | "success" | "warning" | "error";
-	content?: string | ReactNode;
-	delay?: number;
+  className?: string;
+  style?: React.CSSProperties;
+  type?: "information" | "success" | "warning" | "error";
+  content?: string | ReactNode;
+  delay?: number;
 }
 
 const CreateToast: React.FunctionComponent<Props> = (props) => {
-	const { className, type, ...rest } = props;
-	return (
-		<div className={classes("x-toast", className)} {...rest}>
-			<div className="x-toast-icon-wrapper">
-				<Icon name={type!} className={classes("x-toast-icon", type)}></Icon>
-			</div>
-			<div className="x-toast-message">{props.content}</div>
-		</div>
-	);
+  const { className, type, ...rest } = props;
+  return (
+    <div className={classes("x-toast", className)} {...rest}>
+      <div className="x-toast-icon-wrapper">
+        <Icon name={type!} className={classes("x-toast-icon", type)}></Icon>
+      </div>
+      <div className="x-toast-message">{props.content}</div>
+    </div>
+  );
 };
 
 CreateToast.defaultProps = {
-	type: "information",
+  type: "information",
 };
 
 export default CreateToast;
