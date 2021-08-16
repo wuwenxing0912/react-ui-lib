ce9e46d93ccfaa0509859da80558312a8ef7bd1e
wuwenxing <1428560819@qq.com>
2021-08-16 19:48:35 +0800

    格式化 switch 代码

--- lib/switch/switch.tsx
+++ lib/switch/switch.tsx
@@ -3,43 +3,43 @@ import classes from "../helpers/classes";
 import "./switch.scss";
 
 interface Props {
-	style?: React.CSSProperties;
-	checked?: boolean;
-	className?: string;
-	disabled?: boolean;
-	onChecked?: () => void;
+  style?: React.CSSProperties;
+  checked?: boolean;
+  className?: string;
+  disabled?: boolean;
+  onChecked?: () => void;
 }
 
 const Switch: React.FunctionComponent<Props> = (props) => {
-	const { checked, className, disabled, onChecked, ...rest } = props;
-	const checkedClassName = checked ? "switch-checked" : "switch-unchecked";
-	const checkedInnerClassName = checked
-		? "switch-inner-checked"
-		: "switch-inner-unchecked";
-	const disabledClassName = disabled ? "disabled" : undefined;
-	const changeChecked = () => {
-		if (disabled) return;
-		onChecked && onChecked();
-	};
-	return (
-		<span
-			className={classes(
-				"x-switch",
-				checkedClassName,
-				className,
-				disabledClassName
-			)}
-			onClick={() => changeChecked()}
-			{...rest}
-		>
-			<span className={classes("switch-inner", checkedInnerClassName)}></span>
-		</span>
-	);
+  const { checked, className, disabled, onChecked, ...rest } = props;
+  const checkedClassName = checked ? "switch-checked" : "switch-unchecked";
+  const checkedInnerClassName = checked
+    ? "switch-inner-checked"
+    : "switch-inner-unchecked";
+  const disabledClassName = disabled ? "disabled" : undefined;
+  const changeChecked = () => {
+    if (disabled) return;
+    onChecked && onChecked();
+  };
+  return (
+    <span
+      className={classes(
+        "x-switch",
+        checkedClassName,
+        className,
+        disabledClassName
+      )}
+      onClick={() => changeChecked()}
+      {...rest}
+    >
+      <span className={classes("switch-inner", checkedInnerClassName)}></span>
+    </span>
+  );
 };
 
 Switch.defaultProps = {
-	checked: false,
-	disabled: false,
+  checked: false,
+  disabled: false,
 };
 
 export default Switch;
