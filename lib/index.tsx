89ce135ba2119042330a96b1db6d6368c84858b6
wuwenxing <1428560819@qq.com>
2021-08-16 21:36:23 +0800

    发布 1.0.0

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,2 +1,11 @@
 import "./index.scss";
-export { default as Icon } from "./icon/icon";
+export { default as Button } from "./button/button";
+export { default as Form } from "./form/form";
+export { default as Validator } from "./form/validator";
+export { default as Input } from "./input/input";
+export { default as Layout } from "./layout/layout";
+export { default as Modal } from "./modal/modal";
+export { default as Rate } from "./rate/rate";
+export { default as Scroll } from "./scroll/scroll";
+export { default as Switch } from "./switch/switch";
+export { information, success, warning, error } from "./toast/toast";
