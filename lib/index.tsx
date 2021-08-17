5f4d67d29e99591d017d87a56093b12c43a01e16
wuwenxing <1428560819@qq.com>
2021-08-17 10:01:05 +0800

    修改图标未加载的问题

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,5 @@
 import "./index.scss";
+export { default as Icon } from "./icon/icon";
 export { default as Button } from "./button/button";
 export { default as Form } from "./form/form";
 export { default as Validator } from "./form/validator";
