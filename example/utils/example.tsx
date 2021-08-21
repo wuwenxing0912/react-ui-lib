b3cdb488a01736382a4a526e2d23d466d6091185
wuwenxing <1428560819@qq.com>
2021-08-21 22:40:22 +0800

    删除 Fragment

--- example/utils/example.tsx
+++ example/utils/example.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import createHighlightCode from "./code-highlight";
 import Icon from "../../lib/icon/icon";
 
@@ -14,44 +14,42 @@ const Example: React.FunctionComponent<Props> = (props) => {
   const [codeVisible, setCodeVisible] = useState(false);
   const [codeTip, setCodeTip] = useState(false);
   return (
-    <Fragment>
-      <section>
-        {title ? <h1>{title}</h1> : ""}
-        <div className="example-container" style={props.style}>
-          {props.children}
-        </div>
-        <div className="description">
-          <span className="text">{description}</span>
-          <span
-            className="code-icon"
-            style={{ display: "inline-flex", cursor: "pointer" }}
-            onClick={() => {
-              setCodeVisible(!codeVisible);
-            }}
-            onMouseEnter={() => {
-              setCodeTip(!codeTip);
-            }}
-            onMouseLeave={() => {
-              setCodeTip(!codeTip);
-            }}
-          >
-            <Icon name="code" style={{ height: 19 }}></Icon>
-          </span>
-          <span
-            className="pop-content"
-            style={{ display: codeTip ? "inline-block" : "none" }}
-          >
-            {codeVisible ? "收起代码" : "显示代码"}
-          </span>
-        </div>
-        <div
-          className="code-area"
-          style={{ display: codeVisible ? "block" : "none" }}
+    <section>
+      {title ? <h1>{title}</h1> : ""}
+      <div className="example-container" style={props.style}>
+        {props.children}
+      </div>
+      <div className="description">
+        <span className="text">{description}</span>
+        <span
+          className="code-icon"
+          style={{ display: "inline-flex", cursor: "pointer" }}
+          onClick={() => {
+            setCodeVisible(!codeVisible);
+          }}
+          onMouseEnter={() => {
+            setCodeTip(!codeTip);
+          }}
+          onMouseLeave={() => {
+            setCodeTip(!codeTip);
+          }}
         >
-          {createHighlightCode(code as string)}
-        </div>
-      </section>
-    </Fragment>
+          <Icon name="code" style={{ height: 19 }}></Icon>
+        </span>
+        <span
+          className="pop-content"
+          style={{ display: codeTip ? "inline-block" : "none" }}
+        >
+          {codeVisible ? "收起代码" : "显示代码"}
+        </span>
+      </div>
+      <div
+        className="code-area"
+        style={{ display: codeVisible ? "block" : "none" }}
+      >
+        {createHighlightCode(code as string)}
+      </div>
+    </section>
   );
 };
 
